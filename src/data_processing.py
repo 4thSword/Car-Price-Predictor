bd31c046da6424fbada9a1381887c726bb20361e
Jesus Rodriguez <jralvarez1986@gmail.com>
2019-10-04 22:49:18 +0200

    Files loaded

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,5 +1,26 @@
-import pandas as import pd
+# Imports:
 
+import pandas as pd
+
+
+# Global Constants:
+
+TRAIN_PATH = "../input/cars_train.csv"
+TEST_PATH = "../input/cars_test.csv"
+OUTPUT_TRAIN_PATH = "../input/processed_train.csv"
+OUTPUT_TEST_PATH = "../input/processed_test.csv"
+
+# Functions definitions:
+
+def load_data(path):
+    return pd.read_csv(path)
 
 if __name__ == "__main__":
-    pass
+    
+    #First step: To load our training data and our test data:
+    
+    
+    training = load_data(TRAIN_PATH)
+    test = load_data(TEST_PATH)
+    print(training.head())
+    print(test.head())
