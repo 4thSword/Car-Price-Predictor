fd1b1a47b81c915bac994fbb395e8ece58fca5f9
Jesus Rodriguez <jralvarez1986@gmail.com>
2019-10-06 16:27:50 +0200

    Data processing script finished

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,7 +1,8 @@
 # Imports:
 
 import pandas as pd
-
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 # Global Constants:
 
@@ -15,12 +16,53 @@ OUTPUT_TEST_PATH = "../input/processed_test.csv"
 def load_data(path):
     return pd.read_csv(path)
 
+def clean_dataframe(df):
+    # Clean column by column all data to be able to let our model work properly.
+    
+    # Dropping columns that I think that could't give us enogh infomration for our prediction
+    df.drop(['long','lat','county_fips','state_fips','state_code','state_name','city','weather','make','county_name'],axis=1, inplace=True)
+    # Cleaning the cylinders column
+    df['cylinders'] = df.cylinders.str.strip('cylinders')
+    df['cylinders'] = df.cylinders.str.replace('oth', '0')
+    # Cleaning the odometer column filling empty data with de median to try to avoid let the outliers get importance in our predcition
+    df['odometer'] = df.odometer.fillna(df.odometer.median())
+    # Filling nulls in categorical variables by unknown:
+    categorical = ['drive','size','paint_color','type','manufacturer']
+    for category in categorical:
+        df[category] = df[category].fillna('unknown')
+    # filling the empty values of year category with the median in order to don't let our model to be affected by the outliers.
+    df['year'] = df['year'].fillna(training['year'].median())
+    # getting dummies to be able to apply our model
+    df = pd.get_dummies(df)
+    return df
+
+
+
 if __name__ == "__main__":
     
     #First step: To load our training data and our test data:
     
-    
     training = load_data(TRAIN_PATH)
     test = load_data(TEST_PATH)
-    print(training.head())
-    print(test.head())
+    
+
+    #Second step: To clean data before applying standarization:
+
+    training_clean = clean_dataframe(training)
+    test_clean = clean_dataframe(test)
+    print(set(training_clean)-set(test_clean))
+
+    #Third step: Standarization
+    standarize = StandardScaler()
+    standarize.fit(training_clean[['year','odometer']])
+    training_clean[['year','odometer']]= standarize.transform(training_clean[['year','odometer']])
+    test_clean[['year','odometer']]= standarize.transform(test_clean[['year','odometer']])
+
+    # Frouth step: Export data to csv
+    training_clean.to_csv(OUTPUT_TRAIN_PATH)
+    test_clean.to_csv(OUTPUT_TEST_PATH)
+    
+    
+    
+
+    
